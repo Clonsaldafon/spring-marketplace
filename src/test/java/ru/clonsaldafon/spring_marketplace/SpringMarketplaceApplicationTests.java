38f1d0a578f97c65277b4ab669bcee8510fe7ebb
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2025-08-05 19:02:30 +0500

    Created project

---
+++ src/test/java/ru/clonsaldafon/spring_marketplace/SpringMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clonsaldafon.spring_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
